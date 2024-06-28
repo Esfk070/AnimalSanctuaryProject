3cb6fa0bd1ad27f55f889ef853d6d100a8e41afd
Kevin <esfk070@gmail.com>
2024-06-28 16:58:19 -0600

    Add name property to Animal

--- src/com/skilldistillery/sanctuary/Animal.java
+++ src/com/skilldistillery/sanctuary/Animal.java
@@ -1,5 +1,31 @@
 package com.skilldistillery.sanctuary;
 
 public class Animal {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
+
+
+
+//class notes
+//underlined = static
+
+//dont use main, should be just a couple of lines
+
+//UML 
+//Hollow Arrow  = extends = "is a"
+
+// diamond arrows = "has a"
+
+//* is zero or more
+//1 = 1 attendant
